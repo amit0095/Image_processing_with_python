e3e566014e56847718a0e9fd8ac327c6c631a020
amit0095 <85508583+amit0095@users.noreply.github.com>
2021-06-08 02:09:38 +0530

    Update colored_square_box.py

--- colored_square_box.py
+++ colored_square_box.py
@@ -1,13 +1,25 @@
+#import Library
 import cv2
 import numpy as np
+
+#creating black colored new image
 colorpic=np.zeros((500,500,3))
 colorpic[:,:]=[0,0,0]
+
+#white strips in between
 colorpic[:,230:265]=[255,255,255]
 colorpic[230:265,:]=[255,255,255]
+
+#Red colored box
 colorpic[35:195,35:195]=[0,0,255]
+#Sky colored box
 colorpic[300:465,35:195]=[0,255,242]
+#Yellow colored box
 colorpic[35:195,300:465]=[255, 30, 0]
+#Green colored box
 colorpic[300:465,300:465]=[0,255,0]
+
+#Image Showing
 cv2.imshow("Colored_square_box",colorpic)
 cv2.waitKey()
 cv2.destroyAllWindows()
